954e77f152f5eded92d4a83189a5ee04630cb3a0
baliyuvraj <baliyuvraj439@gmail.com>
2024-09-24 06:42:29 +0530

    arrays

---
+++ lectures/08-arrays/code/src/com/assignment/ques2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class ques2{
+    public int[] getConcatinantion(int[] nums){
+        int n = nums.length ; 
+        int[] ans = new int[2 * n];
+
+        for(int i = 0 ; i < n ; i++){
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+    public static void main(String args[]){
+        int[] nums = {1,2,1};
+        ques2 obj = new ques2();
+        System.out.print(Arrays.toString(obj.getConcatinantion(nums))) ;
+    }
+}
