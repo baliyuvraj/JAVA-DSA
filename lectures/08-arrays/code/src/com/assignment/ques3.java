e925323fac743a8517b12091aacc912b1b3fe0ce
baliyuvraj <baliyuvraj439@gmail.com>
2024-09-25 06:08:57 +0530

    arrays

---
+++ lectures/08-arrays/code/src/com/assignment/ques3.java
@@ -0,0 +1,19 @@
+//running sum of 1d array
+import java.util.*;
+class ques3{
+    public int[] runningSum(int[] nums){
+
+        for(int i = 1 ; i < nums.length ; i++){
+            nums[i] += nums[i-1];
+        }
+
+        return nums ; 
+    }
+
+    public static void main(String args[]){
+        int[] nums = {1,2,3,4};
+        ques3 obj = new ques3();
+        System.out.print(Arrays.toString(obj.runningSum(nums)));
+
+    }
+}
---
+++ lectures/08-arrays/code/src/com/assignment/ques4.java
@@ -0,0 +1,26 @@
+//richest customer wealth 
+import java.util.*;
+class ques4{
+    public int maximumWealth(int[][] accounts){
+        int maxWealth = 0 ; 
+        for(int i = 0 ; i<accounts.length ; i++){
+            int currentWealth = 0 ; 
+            for(int j = 0 ; j < accounts[i].length ; j++){
+                currentWealth += accounts[i][j];
+            }
+
+            if(currentWealth > maxWealth){
+                maxWealth = currentWealth ;
+            }
+        }
+        return maxWealth;
+    }
+
+    public static void main(String args[]){
+        int[][] accounts = { {1,5}, {7,3}, {3,5} }; 
+        ques4 obj = new ques4();
+        System.out.print(obj.maximumWealth(accounts)) ;
+
+
+    }
+}
